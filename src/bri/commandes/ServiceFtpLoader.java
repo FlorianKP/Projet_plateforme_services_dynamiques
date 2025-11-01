ec1aa7dbaa5a16f11c2dc9b79ae7b6ee2447bae1
Clement <clementmarteau426@gmail.com>
2025-11-01 11:16:42 +0100

    Verification : package service = identifiant prog

--- src/bri/commandes/ServiceFtpLoader.java
+++ src/bri/commandes/ServiceFtpLoader.java
@@ -1,6 +1,7 @@
 package bri.commandes;
 
 import bri.Service;
+import exceptions.BRiException;
 import programmeurs.Programmeur;
 
 import java.net.MalformedURLException;
@@ -11,8 +12,10 @@ import java.net.URLClassLoader;
 public class ServiceFtpLoader {
     private ServiceFtpLoader() {}
 
-    public static Class<? extends Service> loadServiceClass(String className, Programmeur programmeur) throws ClassNotFoundException, MalformedURLException {
+    public static Class<? extends Service> loadServiceClass(String className, Programmeur programmeur) throws ClassNotFoundException, MalformedURLException, BRiException {
         URLClassLoader urlcl = new URLClassLoader(new URL[]{ new URL(programmeur.getAdresseFtp()) });
+        if (!className.startsWith(programmeur.getIdentifiant() +"."))
+            throw new BRiException("Le nom du package ne correspond pas à votre nom de login");
         return urlcl.loadClass(className).asSubclass(Service.class);
     }
 }
